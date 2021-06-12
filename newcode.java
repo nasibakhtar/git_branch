e198979ecb6292f0ef0319f7586dd36ebc1a50c9
nasibakhtar <85428920+nasibakhtar@users.noreply.github.com>
2021-06-12 21:40:17 +0530

    Update newcode.java

--- newcode.java
+++ newcode.java
@@ -2,5 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello batch21 team");
       System.out.println("Hello batch16 and batch17 students and training practicing and couple of work added let me try one more comment added few more contents ");
+        System.out.println("This is my new repo ");
     }  
 } 
